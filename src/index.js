6def2b66d7f993323c3515ef18f5360bada7762d
Oleg <MNKS@list.ru>
2020-03-24 17:36:45 +1000

    1st stage complete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = '<h1>Hello</h1>';
-
-  return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: './src/index.html',
     }),
   ],
   devServer: {
