55dd1897e929e7abed1906fd0d9b20b694b66119
Oleg <MNKS@list.ru>
2020-03-29 20:11:04 +1000

    added url validator

--- src/js/urlValidator.js
+++ src/js/urlValidator.js
@@ -1,13 +1,31 @@
 const { string, object } = require('yup');
 
-export default (url) => {
+export default (url, pool) => {
   const schema = object().shape({
     url: string().url(),
   });
+
+  if (pool.includes(url)) {
+    const promise = new Promise((resolve) => {
+      const valid = false;
+      const message = 'urls should not be duplicated!';
+
+      resolve({ valid, message });
+    });
+
+    return promise;
+  }
 
   return schema
     .isValid({
       url,
     })
-    .then((valid) => valid);
+    .then((valid) => {
+      if (valid === false) {
+        const message = 'url is not correct';
+        return { valid, message };
+      }
+
+      return { valid, message: null };
+    });
 };
