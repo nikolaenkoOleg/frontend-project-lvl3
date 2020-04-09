ff30ef126d15bd11309317790282fd2017bd8545
Oleg Nikolaenko <MNKS@list.ru>
2020-04-09 11:13:08 +1000

    small refactoring

--- src/js/rssLoader.js
+++ src/js/rssLoader.js
@@ -2,9 +2,5 @@ import axios from 'axios';
 
 export default (url) => axios.get(url)
   .then((response) => response.data)
-  .then((data) => {
-    const parser = new DOMParser();
-    return parser.parseFromString(data, 'text/xml');
-  })
   .then((rss) => rss)
   .catch((error) => error);
--- src/js/rssParser.js
+++ src/js/rssParser.js
@@ -1,7 +1,9 @@
 export default (rss) => {
-  const name = rss.querySelector('title').textContent;
-  const description = rss.querySelector('description').textContent;
-  const posts = rss.querySelectorAll('item');
+  const parser = new DOMParser();
+  const processedRss = parser.parseFromString(rss, 'text/xml');
+  const name = processedRss.querySelector('title').textContent;
+  const description = processedRss.querySelector('description').textContent;
+  const posts = processedRss.querySelectorAll('item');
 
   const postsLinks = [...posts].map((post) => post.querySelector('link').textContent);
   const postsTitles = [...posts].map((post) => post.querySelector('title').textContent);
