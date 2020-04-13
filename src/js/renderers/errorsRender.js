d6625d0712748b45931f2d5787d7ab06a8aee9bf
Oleg <MNKS@list.ru>
2020-04-13 20:03:56 +1000

    refactor err render

--- src/js/renderers/errorsRender.js
+++ src/js/renderers/errorsRender.js
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 
-export default (state, errorType) => {
+export default (state) => {
   const requestErrorBlock = document.querySelector('.error_block');
   const requestErrorContent = document.querySelector('.alert-content');
   const validationUrlFeedbackBlock = document.querySelector('.feedback');
@@ -12,39 +12,30 @@ export default (state, errorType) => {
     e.preventDefault();
 
     // eslint-disable-next-line no-param-reassign
-    state.form.error.requestError = null;
+    state.form.errors.request = null;
   });
 
-  switch (errorType) {
-    case 'validationError':
-      if (state.form.error.validationError) {
-        validationUrlFeedbackBlock.textContent = i18next.t(state.form.error.validationError);
-        validationUrlFeedbackBlock.style.display = 'block';
-        validationUrlFeedbackBlock.classList.add('invalid-feedback');
-        input.classList.add('is-invalid');
-        submit.disabled = true;
-      } else {
-        validationUrlFeedbackBlock.textContent = '';
-        validationUrlFeedbackBlock.style.display = 'none';
-        validationUrlFeedbackBlock.classList.remove('invalid-feedback');
-        input.classList.remove('is-invalid');
-        submit.disabled = false;
-      }
+  if (state.form.errors.validation) {
+    validationUrlFeedbackBlock.textContent = i18next.t(state.form.errors.validation);
+    validationUrlFeedbackBlock.style.display = 'block';
+    validationUrlFeedbackBlock.classList.add('invalid-feedback');
+    input.classList.add('is-invalid');
+    submit.disabled = true;
+  } else {
+    validationUrlFeedbackBlock.textContent = '';
+    validationUrlFeedbackBlock.style.display = 'none';
+    validationUrlFeedbackBlock.classList.remove('invalid-feedback');
+    input.classList.remove('is-invalid');
+    submit.disabled = false;
+  }
 
-      break;
-    case 'requestError':
-      if (state.form.error.requestError) {
-        input.value = '';
-        const errorText = i18next.t(state.form.error.requestError);
-        requestErrorContent.prepend(errorText);
-        requestErrorBlock.style.display = 'block';
-      } else {
-        requestErrorContent.textContent = '';
-        requestErrorBlock.style.display = 'none';
-      }
-
-      break;
-    default:
-      throw new Error(`Unknown error - ${errorType}`);
+  if (state.form.errors.request) {
+    input.value = '';
+    const errorText = i18next.t(state.form.errors.request);
+    requestErrorContent.prepend(errorText);
+    requestErrorBlock.style.display = 'block';
+  } else {
+    requestErrorContent.textContent = '';
+    requestErrorBlock.style.display = 'none';
   }
 };
