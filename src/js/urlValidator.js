4fb7882ca949ac20180854ddc691a69f797e951e
Oleg <MNKS@list.ru>
2020-05-26 21:38:24 +1000

    Update urlValidator.js

--- src/js/urlValidator.js
+++ src/js/urlValidator.js
@@ -1,16 +1,16 @@
 import * as yup from 'yup';
 
-export default (url, urls) => {
-  yup.setLocale({
-    mixed: {
-      notOneOf: 'errors.duplicateUrlError',
-      required: 'errors.requiredUrlError',
-    },
-    string: {
-      url: 'errors.incorrectUrlError',
-    },
-  });
+yup.setLocale({
+  mixed: {
+    notOneOf: 'errors.duplicateUrlError',
+    required: 'errors.requiredUrlError',
+  },
+  string: {
+    url: 'errors.incorrectUrlError',
+  },
+});
 
+export default (url, urls) => {
   const validation = yup.object().shape({
     url: yup
       .string()
